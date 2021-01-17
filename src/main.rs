91af4114b9eb3a257e64ab2d19c66e9f61df047d
Mautar55 <cuanticidadcosmica@live.com>
2021-01-17 09:34:04 -0300

    xfyjxf

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,7 @@ extern crate glium;
 extern crate image;
 
 use glium::glutin::{
-    event::{Event, KeyboardInput, WindowEvent, ElementState},
-    event_loop::{ControlFlow, EventLoop},
-    window::WindowBuilder,
+    event::{KeyboardInput, ElementState},
 };
 
 pub mod mesh; use mesh::*;
@@ -28,6 +26,11 @@ fn main() {
         position: glam::vec3(-0.7,-1.0,0.0),
         ..Default::default()
     };
+
+    let rcubes: Mesh = Mesh::insta_load("resources/meshes/cubes.obj");
+    let rcubes_vlist = rcubes.vertex_list;
+    let rcubes_ilist = rcubes.index_list;
+    let rcubes_transform = Transform::new();
 
     let vp = Viewport::new(640.0, 480.0);
     let mut sample_camera = Camera::new(30.0,vp.ud());
@@ -55,6 +58,9 @@ fn main() {
     let vertex_buffer = glium::VertexBuffer::new(&display, &lista_vertices).unwrap();
     let index_buffer = glium::IndexBuffer::new(&display, glium::index::PrimitiveType::TrianglesList,&lista_indices).unwrap();
 
+    let rcubes_vbuffer = glium::VertexBuffer::new(&display, &rcubes_vlist).unwrap();
+    let rcubes_ibuffer =glium::IndexBuffer::new(&display, glium::index::PrimitiveType::TrianglesList, &rcubes_ilist).unwrap();
+    
     let vertex_shader_src = fs::read_to_string("resources/shaders/vert-shader.glsl")
         .expect("\n### No se encontro el archivo vertex shader. \n");
     let fragment_shader_src = fs::read_to_string("resources/shaders/frag-shader.glsl")
@@ -88,7 +94,7 @@ fn main() {
                     },
                     ..
                  } => match virtual_code {
-                     Q => sample_camera.set_roll_deg(180.0),
+                     glium::glutin::event::VirtualKeyCode::Q => sample_camera.set_roll_deg(180.0),
                      _ => return
                  },
                 glutin::event::WindowEvent::CloseRequested => {
@@ -159,6 +165,25 @@ fn main() {
         target.draw(
             &vertex_buffer,
             &index_buffer,
+            &program,
+            &uniforms,
+            &params,
+        ).unwrap();
+
+        ///////////// DRAWCALL DE LOS CUBES
+        
+        let transform_matrix = rcubes_transform.get_transform_matrix();
+
+        let uniforms = uniform! {
+            t_matrix: transform_matrix,
+            p_matrix: perspective_matrix,
+            v_matrix: view_matrix,
+            u_light: light,
+        };
+        
+        target.draw(
+            &rcubes_vbuffer,
+            &rcubes_ibuffer,
             &program,
             &uniforms,
             &params,
